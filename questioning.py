81eda7c75482dd298c3306c7f124f16739a5a184
Abhiram R <abhi.darkness@gmail.com>
2022-01-03 02:22:58 +0530

    Adding San thingys5

--- questioning.py
+++ questioning.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown(":santa:")
 
-st.title("Broke Bibliophiles Bangalore Secret Santa 2021 - Homenum Revelio !", anchor=None)
+
+st.title(st.markdown(":santa:")+"Broke Bibliophiles Bangalore Secret Santa 2021 - Homenum Revelio !", anchor=None)
 
 df = pd.read_csv("./BBBSecretSanta 2021_W.csv")
 st.table(df)
